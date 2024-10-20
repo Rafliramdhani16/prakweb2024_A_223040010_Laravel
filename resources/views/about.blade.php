b2db1ba01ed7181e27ce7f7f2b7fa114ddb56d87
Rafliramdhani16 <rafliramdhani773@gmail.com>
2024-10-20 20:20:04 +0700

    laravel #4

---
+++ resources/views/about.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman About</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/blog">About</a>
+    <a href="/contact">About</a>
+    <h1>Halaman About</h1>
+    <h3>Owener : {{$nama}}</h3>
+    <img src="img/senku.jpg" alt="" width="200">
+</body>
+</html>
