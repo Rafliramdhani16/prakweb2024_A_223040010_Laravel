cd817c0b4ef9da7f1bbba36cf5c72f57ac4fb5a6
Rafliramdhani16 <rafliramdhani773@gmail.com>
2024-10-20 22:00:08 +0700

    laravel #5

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,27 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Halaman Contact</title>
-</head>
-<body>
-    <a href="/">Home</a>
-    <a href="/about">About</a>
-    <a href="/blog">About</a>
-    <a href="/contact">About</a>
-    <h1>Halaman Contact</h1>
-    <ul>
-        <li>
-            <a href="https://intagram.com/rafli_ramdhanii">Intagram</a>
-        </li>
-        <li>
-            <a href="https://x.com/rafli_ramdhanii">X</a>
-        </li>
-        <li>
-            <a href="https://youtube.com/rafli_ramdhanii">You Tube</a>
-        </li>
-    </ul>
-</body>
-</html>
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3 class="text-xl">Welcome to my Blogsd</h3>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,15 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', ['title' => 'Home Page']);
 });
 
 Route::get('/about', function () {
-    return view('about', ['nama'=>'Rafli Ramdhani']);
+    return view('about', ['name'=>'Rafli Ramdhani', 'title' => 'About']);
 });
 Route::get('/blog', function () {
-    return view('blog');
+    return view('blog', ['title' => 'Blog']);
 });
 Route::get('/contact', function () {
-    return view('contact');
+    return view('contact', ['title' => 'Contact']);
 });
