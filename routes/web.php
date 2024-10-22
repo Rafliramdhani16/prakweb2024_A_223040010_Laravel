7c80585fc77bf37275628004358340d4a8710a16
Rafliramdhani16 <rafliramdhani773@gmail.com>
2024-10-22 07:55:01 +0700

    Laravel #6

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,9 +10,50 @@ Route::get('/', function () {
 Route::get('/about', function () {
     return view('about', ['name'=>'Rafli Ramdhani', 'title' => 'About']);
 });
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog', 'posts' =>
+[
+    [
+        'id'=> '1',
+        'slug' => 'judul-artikel-1',
+        'title' => 'Artikel 1',
+        'author' => 'Rafli Ramdhani',
+        'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima eligendi, sit ducimus vero, fugiat perspiciatis cumque sapiente ut placeat iusto beatae! Voluptate cupiditate optio provident ipsam numquam quam obcaecati architecto?'
+    ],
+    [
+        'id'=> '2',
+        'slug' => 'judul-artikel-2',
+        'title' => 'Artikel 2',
+        'author' => 'Rafli Ramdhani',
+        'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis enim ipsum laudantium expedita placeat labore autem excepturi inventore fugiat nisi quaerat, cum eaque tempore delectus perferendis vitae. Beatae, ea atque!'
+    ],
+]]);
 });
+
+Route::get('/posts/{slug}', function($slug){
+    $posts = [
+        [
+            'id'=> '1',
+            'slug' => 'judul-artikel-1',
+            'title' => 'Artikel 1',
+            'author' => 'Rafli Ramdhani',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima eligendi, sit ducimus vero, fugiat perspiciatis cumque sapiente ut placeat iusto beatae! Voluptate cupiditate optio provident ipsam numquam quam obcaecati architecto?'
+        ],
+        [
+            'id'=> '2',
+            'slug' => 'judul-artikel-2',
+            'title' => 'Artikel 2',
+            'author' => 'Rafli Ramdhani',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis enim ipsum laudantium expedita placeat labore autem excepturi inventore fugiat nisi quaerat, cum eaque tempore delectus perferendis vitae. Beatae, ea atque!'
+        ],
+    ];
+
+    $post = Arr::first($posts, function($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
+});
+
 Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact']);
 });
