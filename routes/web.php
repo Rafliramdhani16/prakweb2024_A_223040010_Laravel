29ad8c2f377013419b59b7639bcdeed7bdb0ddb4
Rafliramdhani16 <rafliramdhani773@gmail.com>
2024-10-25 02:03:00 +0700

    Laravel #11

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 
@@ -17,11 +18,13 @@ Route::get('/posts', function () {
 });
 
 Route::get('/posts/{post:slug}', function(Post $post){
-
     // $post = Post::find($slug);
     return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
-
+Route::get('/authors/{user}', function(User $user){
+    // $post = Post::find($slug);
+    return view('posts', ['title' => 'Articles By ' . $user->name, 'posts' => $user->posts]);
+});
 Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact']);
 });
