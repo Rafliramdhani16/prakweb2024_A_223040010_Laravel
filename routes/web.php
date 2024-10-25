cd47a466616b88a225e7abd239c1caddd2c38068
Rafliramdhani16 <rafliramdhani773@gmail.com>
2024-10-25 22:15:57 +0700

    Laravel #14

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,10 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
-    return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
+    // melakukan query dengan eager loading
+    // $posts = Post::with(['author', 'category'])->latest()->get();
+    $posts = Post::latest()->get();
+    return view('posts', ['title' => 'Blog', 'posts' => $posts]);
 });
 
 Route::get('/posts/{post:slug}', function(Post $post){
@@ -25,6 +28,9 @@ Route::get('/posts/{post:slug}', function(Post $post){
 });
 
 Route::get('/authors/{user:username}', function(User $user) {
+    // melakukan eager loading pada author dan category pada post yang dimiliki oleh user
+    // $posts = $user->posts->load('category', 'author');
+    
     return view('posts', [
         'title' => count($user->posts) . ' Articles By ' . $user->name,
         'posts' => $user->posts
@@ -32,6 +38,7 @@ Route::get('/authors/{user:username}', function(User $user) {
 });
 
 Route::get('/categories/{category:slug}', function(Category $category){
+    // $posts = $category->posts->load('category', 'author');
     return view ('posts', ['title' => 'Article in :' . $category->name, 'posts' => $category->posts]);
 });
 
